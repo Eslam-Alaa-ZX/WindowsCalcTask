0cb1802f014bed2753577a043520391e5dcb7a4d
Eslam Alaa <zoro12zx@gmail.com>
2022-11-10 19:11:13 +0200

    Eslam Alaa-Calc end

---
+++ Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace WindowsTask1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Number7 = new System.Windows.Forms.Button();
+            this.Number8 = new System.Windows.Forms.Button();
+            this.Number9 = new System.Windows.Forms.Button();
+            this.Number4 = new System.Windows.Forms.Button();
+            this.Number5 = new System.Windows.Forms.Button();
+            this.Number6 = new System.Windows.Forms.Button();
+            this.Number1 = new System.Windows.Forms.Button();
+            this.Number2 = new System.Windows.Forms.Button();
+            this.Number3 = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.min = new System.Windows.Forms.Button();
+            this.CE = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.Number0 = new System.Windows.Forms.Button();
+            this.dote = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.eq = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Number7
+            // 
+            this.Number7.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number7.Location = new System.Drawing.Point(52, 97);
+            this.Number7.Name = "Number7";
+            this.Number7.Size = new System.Drawing.Size(75, 75);
+            this.Number7.TabIndex = 0;
+            this.Number7.Text = "7";
+            this.Number7.UseVisualStyleBackColor = true;
+            this.Number7.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // Number8
+            // 
+            this.Number8.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number8.Location = new System.Drawing.Point(142, 97);
+            this.Number8.Name = "Number8";
+            this.Number8.Size = new System.Drawing.Size(75, 75);
+            this.Number8.TabIndex = 0;
+            this.Number8.Text = "8";
+            this.Number8.UseVisualStyleBackColor = true;
+            this.Number8.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // Number9
+            // 
+            this.Number9.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number9.Location = new System.Drawing.Point(232, 97);
+            this.Number9.Name = "Number9";
+            this.Number9.Size = new System.Drawing.Size(75, 75);
+            this.Number9.TabIndex = 0;
+            this.Number9.Text = "9";
+            this.Number9.UseVisualStyleBackColor = true;
+            this.Number9.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // Number4
+            // 
+            this.Number4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number4.Location = new System.Drawing.Point(52, 178);
+            this.Number4.Name = "Number4";
+            this.Number4.Size = new System.Drawing.Size(75, 75);
+            this.Number4.TabIndex = 0;
+            this.Number4.Text = "4";
+            this.Number4.UseVisualStyleBackColor = true;
+            this.Number4.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // Number5
+            // 
+            this.Number5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number5.Location = new System.Drawing.Point(142, 178);
+            this.Number5.Name = "Number5";
+            this.Number5.Size = new System.Drawing.Size(75, 75);
+            this.Number5.TabIndex = 0;
+            this.Number5.Text = "5";
+            this.Number5.UseVisualStyleBackColor = true;
+            this.Number5.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // Number6
+            // 
+            this.Number6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number6.Location = new System.Drawing.Point(232, 178);
+            this.Number6.Name = "Number6";
+            this.Number6.Size = new System.Drawing.Size(75, 75);
+            this.Number6.TabIndex = 0;
+            this.Number6.Text = "6";
+            this.Number6.UseVisualStyleBackColor = true;
+            this.Number6.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // Number1
+            // 
+            this.Number1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number1.Location = new System.Drawing.Point(52, 259);
+            this.Number1.Name = "Number1";
+            this.Number1.Size = new System.Drawing.Size(75, 75);
+            this.Number1.TabIndex = 0;
+            this.Number1.Text = "1";
+            this.Number1.UseVisualStyleBackColor = true;
+            this.Number1.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // Number2
+            // 
+            this.Number2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number2.Location = new System.Drawing.Point(142, 259);
+            this.Number2.Name = "Number2";
+            this.Number2.Size = new System.Drawing.Size(75, 75);
+            this.Number2.TabIndex = 0;
+            this.Number2.Text = "2";
+            this.Number2.UseVisualStyleBackColor = true;
+            this.Number2.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // Number3
+            // 
+            this.Number3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number3.Location = new System.Drawing.Point(232, 259);
+            this.Number3.Name = "Number3";
+            this.Number3.Size = new System.Drawing.Size(75, 75);
+            this.Number3.TabIndex = 0;
+            this.Number3.Text = "3";
+            this.Number3.UseVisualStyleBackColor = true;
+            this.Number3.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // div
+            // 
+            this.div.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.div.Location = new System.Drawing.Point(313, 97);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(75, 75);
+            this.div.TabIndex = 0;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.op_Click);
+            // 
+            // mul
+            // 
+            this.mul.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mul.Location = new System.Drawing.Point(313, 178);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(75, 75);
+            this.mul.TabIndex = 0;
+            this.mul.Text = "X";
+            this.mul.UseVisualStyleBackColor = true;
+            this.mul.Click += new System.EventHandler(this.op_Click);
+            // 
+            // min
+            // 
+            this.min.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.min.Location = new System.Drawing.Point(313, 259);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(75, 75);
+            this.min.TabIndex = 0;
+            this.min.Text = "-";
+            this.min.UseVisualStyleBackColor = true;
+            this.min.Click += new System.EventHandler(this.op_Click);
+            // 
+            // CE
+            // 
+            this.CE.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CE.Location = new System.Drawing.Point(394, 97);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(75, 75);
+            this.CE.TabIndex = 0;
+            this.CE.Text = "CE";
+            this.CE.UseVisualStyleBackColor = true;
+            this.CE.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // C
+            // 
+            this.C.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C.Location = new System.Drawing.Point(394, 178);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(75, 75);
+            this.C.TabIndex = 0;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // Number0
+            // 
+            this.Number0.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number0.Location = new System.Drawing.Point(52, 340);
+            this.Number0.Name = "Number0";
+            this.Number0.Size = new System.Drawing.Size(165, 75);
+            this.Number0.TabIndex = 0;
+            this.Number0.Text = "0";
+            this.Number0.UseVisualStyleBackColor = true;
+            this.Number0.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // dote
+            // 
+            this.dote.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dote.Location = new System.Drawing.Point(232, 340);
+            this.dote.Name = "dote";
+            this.dote.Size = new System.Drawing.Size(75, 75);
+            this.dote.TabIndex = 0;
+            this.dote.Text = ".";
+            this.dote.UseVisualStyleBackColor = true;
+            this.dote.Click += new System.EventHandler(this.Number_Click);
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(313, 340);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 75);
+            this.add.TabIndex = 0;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.op_Click);
+            // 
+            // eq
+            // 
+            this.eq.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eq.Location = new System.Drawing.Point(394, 259);
+            this.eq.Name = "eq";
+            this.eq.Size = new System.Drawing.Size(75, 156);
+            this.eq.TabIndex = 0;
+            this.eq.Text = "=";
+            this.eq.UseVisualStyleBackColor = true;
+            this.eq.Click += new System.EventHandler(this.eq_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(52, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(417, 79);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.CE);
+            this.Controls.Add(this.eq);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.Number3);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.Number6);
+            this.Controls.Add(this.Number9);
+            this.Controls.Add(this.dote);
+            this.Controls.Add(this.Number2);
+            this.Controls.Add(this.Number5);
+            this.Controls.Add(this.Number8);
+            this.Controls.Add(this.Number0);
+            this.Controls.Add(this.Number1);
+            this.Controls.Add(this.Number4);
+            this.Controls.Add(this.Number7);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Number7;
+        private System.Windows.Forms.Button Number8;
+        private System.Windows.Forms.Button Number9;
+        private System.Windows.Forms.Button Number4;
+        private System.Windows.Forms.Button Number5;
+        private System.Windows.Forms.Button Number6;
+        private System.Windows.Forms.Button Number1;
+        private System.Windows.Forms.Button Number2;
+        private System.Windows.Forms.Button Number3;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button mul;
+        private System.Windows.Forms.Button min;
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button Number0;
+        private System.Windows.Forms.Button dote;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button eq;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsTask1
+{
+    public partial class Form1 : Form
+    {
+        string op = "";
+        double val = 0;
+        bool isOp = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Number_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if(textBox1.Text == "0" || isOp)
+            {
+                if (btn.Text == ".")
+                {
+                    textBox1.Text += btn.Text;
+                    return;
+                }
+                else
+                {
+                    textBox1.Clear();
+                }
+                
+            }
+            if(btn.Text == ".")
+            {
+                if (!textBox1.Text.Contains("."))
+                {
+                    textBox1.Text += btn.Text;
+                }
+            }
+            else
+            {
+                textBox1.Text += btn.Text;
+            }
+            isOp = false;
+
+        }
+
+        private void op_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            op = btn.Text;
+            val = double.Parse(textBox1.Text);
+            isOp = true;
+
+        }
+
+        private void eq_Click(object sender, EventArgs e)
+        {
+            switch (op)
+            {
+                case "/":
+                    textBox1.Text = (val / double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "X":
+                    textBox1.Text = (val * double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "-":
+                    textBox1.Text = (val - double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "+":
+                    textBox1.Text = (val + double.Parse(textBox1.Text)).ToString();
+                    break;
+
+            }
+        }
+
+        private void CE_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            val = 0;
+        }
+    }
+}
